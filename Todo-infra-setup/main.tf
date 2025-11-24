a0867a547084c3b74b481df03ebc18f7050527ce
prakashjay1993 <cloudtechhacks@gmail.com>
2025-11-24 21:46:43 +0530

    102-vnet-dummy02 add

--- Todo-infra-setup/main.tf
+++ Todo-infra-setup/main.tf
@@ -36,6 +36,16 @@ module "todo-vnet" {
   address_space       = ["10.10.10.0/24"]
 
 }
+#ticket102-vnet-dummy02 added
+module "todo-vnet" {
+  depends_on          = [module.todo-rg]
+  source              = "../../Child_module/azurerm_azure_network"
+  vnet_name           = "vnet-dummy02"
+  vnet_location       = "Central India"
+  resource_group_name = "dev-todo-rg"
+  address_space       = ["10.10.10.0/24"]
+
+}
 #dard-1:Subnet frontend & backend ke lie alg-2 module bna rhe hai
 module "todo-frontend-subnet" {
   depends_on           = [module.todo-vnet]
