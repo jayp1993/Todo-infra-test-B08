ee8d067c85d68f82f3b7dacc91248c9a1d8d6419
prakashjay1993 <cloudtechhacks@gmail.com>
2025-11-25 21:27:18 +0530

    rg-tondhu added and variable.tf deleted

--- Todo-infra-setup/main.tf
+++ Todo-infra-setup/main.tf
@@ -1,10 +1,15 @@
 module "todo-rg" {
-  for_each = var.rg_details
   source      = "../../Child_module/azurerm_resource_group"
   rg_name     ="todo-rg"
   rg_location = "central India"
 }
 
+#ticket102:rg-tondhu added 
+module "rg-thondhu" {
+  source      = "../../Child_module/azurerm_resource_group"
+  rg_name     ="rg-thonthu"
+  rg_location = "central India"
+}
 
 module "todo-vnet" {
   depends_on          = [module.todo-rg]
