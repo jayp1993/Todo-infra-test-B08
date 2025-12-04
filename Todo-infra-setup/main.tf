15f929767a2841eda2fda068a09403a705d527f2
Cloud-Tech-Hacks <jayp.may1993@outlook.com>
2025-12-04 22:09:24 +0530

    101-todo-cus-rg-task-done (#8)
    Co-authored-by: prakashjay1993 <cloudtechhacks@gmail.com>

--- Todo-infra-setup/main.tf
+++ Todo-infra-setup/main.tf
@@ -4,5 +4,10 @@ module "todo-rg" {
   rg_location = "central India"
 }
 
+module "rg-cus-todo" {
+source      = "../Module/azurerm_resource_group"
+  rg_name     ="todo-cus-rg"
+  rg_location = "central US"
+}  
 
 
