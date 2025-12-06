4f045f45261de05765c259be46961a07c36802cf
Lokesh kumar <lokesh_kumar95@yahoo.com>
2025-12-06 19:52:05 +0530

    todo-rd-06Dec2025

--- Todo-infra-setup/main.tf
+++ Todo-infra-setup/main.tf
@@ -10,4 +10,10 @@ source      = "../Module/azurerm_resource_group"
   rg_location = "central US"
 }  
 
+module "rg-06Dec2025" {
+source      = "../Module/azurerm_resource_group"
+  rg_name     ="todo-06Dec2025-rg"
+  rg_location = "Central US"
+  
+}
 
