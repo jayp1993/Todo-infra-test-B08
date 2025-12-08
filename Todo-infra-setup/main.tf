5326aec1ca1f5ac9b415e750f4bb9a169ed71b36
Cloud-Tech-Hacks <jayp.may1993@outlook.com>
2025-12-08 22:58:38 +0530

    Update main.tf

--- Todo-infra-setup/main.tf
+++ Todo-infra-setup/main.tf
@@ -1,19 +1,1 @@
-module "todo-rg" {
-  source      = "../Module/azurerm_resource_group"
-  rg_name     ="todo-rg"
-  rg_location = "central India"
-}
-
-module "rg-cus-todo" {
-source      = "../Module/azurerm_resource_group"
-  rg_name     ="todo-cus-rg"
-  rg_location = "central US"
-}  
-
-module "rg-06Dec2025" {
-source      = "../Module/azurerm_resource_group"
-  rg_name     ="todo-06Dec2025-rg"
-  rg_location = "Central US"
-  
-}
 
